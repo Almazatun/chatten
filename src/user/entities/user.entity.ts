cfba638184f826eedde12326b06966259c6527f3
Almazatun <atunstyle@gmail.com>
2022-05-30 22:37:44 +0600

    add new colums in user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+import { Message } from "../../message/entities/message.entity";
+import { GroupMember } from "../../group-member/entities/group-member.entity";
 
 @Entity("users")
 export class User {
@@ -13,4 +16,16 @@ export class User {
 
   @Column({ select: false })
   public password!: string;
+
+  @Column({ nullable: true })
+  public createdAt!: Date;
+
+  @Column({ nullable: true })
+  public updatedAt!: Date;
+
+  @OneToMany(() => Message, message => message.user)
+  public messages: Message[];
+
+  @OneToMany(() => GroupMember, groupMember => groupMember.user)
+  public groupMembers: GroupMember[];
 }
